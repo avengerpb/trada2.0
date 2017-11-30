1b38b47fccabdfd1b1beb2e8a9e98c2e3c76f543
Duc Nguyen <nguyenminhduc1803@gmail.com>
2017-11-30 03:34:37 +0000

    Running node server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongojs = require("mongojs");
+const expressValidator = require("express-validator");
+
+let index = require("./routes/index");
+let app = express();
+app.set('port', (process.env.PORT || 5000));
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+//Set static path
+app.use(express.static(path.join(__dirname, 'client')));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+
+
+app.listen(app.get('port'), function() {
+  console.log(`Node app is running at port: ${app.get('port')}`);
+});
